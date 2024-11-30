86ff4b5f820f4e991ad717fc704f0ca769872529
Ned Loynd <ned@loynd.com>
2024-11-30 15:15:45 +1100

    Use envelope calculations from 3DSUSoundArchiveTool

--- external/vgmtrans/SF2File.cpp
+++ external/vgmtrans/SF2File.cpp
@@ -34,93 +34,63 @@ static float GetFallingRate(uint8_t DecayTime) {
     return ((60.0f) / (126 - DecayTime)) / 5.0f;
 }
 
+// taken from https://github.com/soneek/3DSUSoundArchiveTool
+static double LogB( double n, double b ) {
+  // log(n)/log(2) is log2.
+  return log( n ) / log( b );
+}
+
+// taken from https://github.com/soneek/3DSUSoundArchiveTool
+static double timeToTimecents(double time){
+  double timeCent = floor(1200 * LogB(time, 2));
+  if (timeCent > -12000 && timeCent < 0) {
+    return timeCent + 65536;
+  } else if (timeCent < -12000) {
+    return -12000;
+  } else {
+    return timeCent;
+  }
+}
+
 static EnvelopeParams envelopeFromInfo(const rsnd::InstrInfo* info) {
+  // mostly taken from https://github.com/soneek/3DSUSoundArchiveTool
   EnvelopeParams envelope;
 
-  static const float attackTable[128] = {
-    0.9992175f, 0.9984326f, 0.9976452f, 0.9968553f,
-    0.9960629f, 0.9952679f, 0.9944704f, 0.9936704f,
-    0.9928677f, 0.9920625f, 0.9912546f, 0.9904441f,
-    0.9896309f, 0.9888151f, 0.9879965f, 0.9871752f,
-    0.9863512f, 0.9855244f, 0.9846949f, 0.9838625f,
-    0.9830273f, 0.9821893f, 0.9813483f, 0.9805045f,
-    0.9796578f, 0.9788081f, 0.9779555f, 0.9770999f,
-    0.9762413f, 0.9753797f, 0.9745150f, 0.9736472f,
-    0.9727763f, 0.9719023f, 0.9710251f, 0.9701448f,
-    0.9692612f, 0.9683744f, 0.9674844f, 0.9665910f,
-    0.9656944f, 0.9647944f, 0.9638910f, 0.9629842f,
-    0.9620740f, 0.9611604f, 0.9602433f, 0.9593226f,
-    0.9583984f, 0.9574706f, 0.9565392f, 0.9556042f,
-    0.9546655f, 0.9537231f, 0.9527769f, 0.9518270f,
-    0.9508732f, 0.9499157f, 0.9489542f, 0.9479888f,
-    0.9470195f, 0.9460462f, 0.9450689f, 0.9440875f,
-    0.9431020f, 0.9421124f, 0.9411186f, 0.9401206f,
-    0.9391184f, 0.9381118f, 0.9371009f, 0.9360856f,
-    0.9350659f, 0.9340417f, 0.9330131f, 0.9319798f,
-    0.9309420f, 0.9298995f, 0.9288523f, 0.9278004f,
-    0.9267436f, 0.9256821f, 0.9246156f, 0.9235442f,
-    0.9224678f, 0.9213864f, 0.9202998f, 0.9192081f,
-    0.9181112f, 0.9170091f, 0.9159016f, 0.9147887f,
-    0.9136703f, 0.9125465f, 0.9114171f, 0.9102821f,
-    0.9091414f, 0.9079949f, 0.9068427f, 0.9056845f,
-    0.9045204f, 0.9033502f, 0.9021740f, 0.9009916f,
-    0.8998029f, 0.8986080f, 0.8974066f, 0.8961988f,
-    0.8949844f, 0.8900599f, 0.8824622f, 0.8759247f,
-    0.8691861f, 0.8636406f, 0.8535788f, 0.8430189f,
-    0.8286135f, 0.8149099f, 0.8002172f, 0.7780663f,
-    0.7554750f, 0.7242125f, 0.6828239f, 0.6329169f,
-    0.5592135f, 0.4551411f, 0.3298770f, 0.0000000f
-  };
-  
-  /* Figure out how many msecs it takes to go from the initial decimal to our threshold. */
-  float realAttack = attackTable[info->attack];
-  int msecs = 0;
+  double attackTable[] = {13122, 6546, 4356, 3261, 2604, 2163, 1851, 1617, 1434, 1287, 1167, 1068, 984, 912, 849, 795, 747, 702, 666, 630, 600, 570, 543, 519, 498, 477, 459, 441, 426, 411, 396, 384, 372, 360, 348, 336, 327, 318, 309, 300, 294, 285, 279, 270, 264, 258, 252, 246, 240, 234, 231, 225, 219, 216, 210, 207, 201, 198, 195, 192, 186, 183, 180, 177, 174, 171, 168, 165, 162, 159, 156, 153.5, 153, 150, 147, 144, 141.5, 141, 138, 135.5, 135, 132, 129.5, 129, 126, 123.5, 123, 120.5, 120, 117, 114.5, 114, 111.5, 111, 108.5, 108, 105.7, 105.35, 105, 102.5, 102, 99.5, 99, 96.7, 96.35, 96, 93.5, 93, 90, 87, 81, 75, 72, 69, 63, 60, 54, 48, 45, 39, 36, 30, 24, 21, 15, 12, 9, 6.1e-6};
+  double holdTable[] = {6e-6, 1, 2, 4, 6, 9, 12, 16, 20, 25, 30, 36, 42, 49, 56, 64, 72, 81, 90, 100, 110, 121, 132, 144, 156, 169, 182, 196, 210, 225, 240, 256, 272, 289, 306, 324, 342, 361, 380, 400, 420, 441, 462, 484, 506, 529, 552, 576, 600, 625, 650, 676, 702, 729, 756, 784, 812, 841, 870, 900, 930, 961, 992, 1024, 1056, 1089, 1122, 1156, 1190, 1225, 1260, 1296, 1332, 1369, 1406, 1444, 1482, 1521, 1560, 1600, 1640, 1681, 1722, 1764, 1806, 1849, 1892, 1936, 1980, 2025, 2070, 2116, 2162, 2209, 2256, 2304, 2352, 2401, 2450, 2500, 2550, 2601, 2652, 2704, 2756, 2809, 2862, 2916, 2970, 3025, 3080, 3136, 3192, 3249, 3306, 3364, 3422, 3481, 3540, 3600, 3660, 3721, 3782, 3844, 3906, 3969, 4032, 4096};
+  double decayTable[] = { -0.00016, -0.00047, -0.00078, -0.00109, -0.00141, -0.00172, -0.00203, -0.00234, -0.00266, -0.00297, -0.00328, -0.00359, -0.00391, -0.00422, -0.00453, -0.00484, -0.00516, -0.00547, -0.00578, -0.00609, -0.00641, -0.00672, -0.00703, -0.00734, -0.00766, -0.00797, -0.00828, -0.00859, -0.00891, -0.00922, -0.00953, -0.00984, -0.01016, -0.01047, -0.01078, -0.01109, -0.01141, -0.01172, -0.01203, -0.01234, -0.01266, -0.01297, -0.01328, -0.01359, -0.01391, -0.01422, -0.01453, -0.01484, -0.01516, -0.01547, -0.01579, -0.016, -0.01622, -0.01644, -0.01667, -0.0169, -0.01714, -0.01739, -0.01765, -0.01791, -0.01818, -0.01846, -0.01875, -0.01905, -0.01935, -0.01967, -0.02, -0.02034, -0.02069, -0.02105, -0.02143, -0.02182, -0.02222, -0.02264, -0.02308, -0.02353, -0.024, -0.02449, -0.025, -0.02553, -0.02609, -0.02667, -0.02727, -0.02791, -0.02857, -0.02927, -0.03, -0.03077, -0.03158, -0.03243, -0.03333, -0.03429, -0.03529, -0.03636, -0.0375, -0.03871, -0.04, -0.04138, -0.04286, -0.04444, -0.04615, -0.048, -0.05, -0.05217, -0.05455, -0.05714, -0.06, -0.06316, -0.06667, -0.07059, -0.075, -0.08, -0.08571, -0.09231, -1, -0.10909, -0.12, -0.13333, -0.15, -0.17143, -2, -2.4, -3, -4, -6, -12, -24, -65535 };
 
-  const float VOLUME_DB_MIN = -90.4f;
+  envelope.attack_time = timeToTimecents(attackTable[info->attack] / 1000);
 
-  float vol = VOLUME_DB_MIN * 10.0f;
-  while (vol <= -1.0f / 32.0f) {
-    vol *= realAttack;
-    msecs++;
+  double sustainVol = 20 * LogB(pow(((double)info->sustain / 127.0000), 2), 10);
+
+  if (info->decay == 127) {
+    envelope.decay_time = -12000;
+  } else {
+    if (info->sustain == 0) {
+      envelope.decay_time = timeToTimecents(-90.25 / decayTable[info->decay] / 1000);
+    } else {
+      envelope.decay_time = timeToTimecents(sustainVol / decayTable[info->decay] / 1000);
+    }
   }
-  envelope.attack_time = msecs / 1000.0;
 
-  /* Scale is dB*10, so the first number is actually -72.3dB.
-   * Minimum possible volume is -90.4dB. */
-  const int16_t sustainTable[] = {
-    -723, -722, -721, -651, -601, -562, -530, -503,
-    -480, -460, -442, -425, -410, -396, -383, -371,
-    -360, -349, -339, -330, -321, -313, -305, -297,
-    -289, -282, -276, -269, -263, -257, -251, -245,
-    -239, -234, -229, -224, -219, -214, -210, -205,
-    -201, -196, -192, -188, -184, -180, -176, -173,
-    -169, -165, -162, -158, -155, -152, -149, -145,
-    -142, -139, -136, -133, -130, -127, -125, -122,
-    -119, -116, -114, -111, -109, -106, -103, -101,
-    -99,  -96,  -94,  -91,  -89,  -87,  -85,  -82,
-    -80,  -78,  -76,  -74,  -72,  -70,  -68,  -66,
-    -64,  -62,  -60,  -58,  -56,  -54,  -52,  -50,
-    -49,  -47,  -45,  -43,  -42,  -40,  -38,  -36,
-    -35,  -33,  -31,  -30,  -28,  -27,  -25,  -23,
-    -22,  -20,  -19,  -17,  -16,  -14,  -13,  -11,
-    -10,  -8,   -7,   -6,   -4,   -3,   -1,    0
-  };
+  if (info->sustain == 0) {
+    envelope.sustain_level = 900;
+  } else {
+    envelope.sustain_level = 200 * abs(LogB(pow(((double)info->sustain / 127.0000), 2), 10));
+  }
 
-  float decayRate = GetFallingRate(info->decay);
-  int16_t sustainLev = sustainTable[info->sustain];
+  if (info->release == 127) {
+    envelope.release_time = -12000;
+  } else {
+    if (info->sustain == 0) {
+			envelope.release_time = timeToTimecents(-90.25 / decayTable[info->release] / 1000);
+    } else {
+			envelope.release_time = timeToTimecents((-90.25 - sustainVol) / decayTable[info->release] / 1000);
+    }
+  }
 
-  /* Decay time is the time it takes to get to the sustain level from max vol,
-   * decaying by decayRate every 1ms. */
-  envelope.decay_time = ((0.0f - sustainLev) / decayRate) / 1000.0;
-
-  envelope.sustain_level = 1.0 - (sustainLev / VOLUME_DB_MIN);
-
-  float releaseRate = GetFallingRate(info->release);
-
-  /* Release time is the time it takes to get from sustain level to minimum volume. */
-  envelope.release_time = ((sustainLev - VOLUME_DB_MIN) / releaseRate) / 1000.0;
-
-  envelope.hold_time = (info->hold + 1) * (info->hold + 1) / 4000.0;
+  envelope.hold_time = timeToTimecents(holdTable[info->hold] / 1000);
 
   return envelope;
 }
@@ -425,37 +395,31 @@ SF2File::SF2File(const rsnd::SoundBank *bankfile, const std::vector<WaveAudio>& 
 
       // attackVolEnv
       instGenList.sfGenOper = attackVolEnv;
-      instGenList.genAmount.shAmount =
-          (envelope.attack_time == 0) ? -32768 : std::round(SecondsToTimecents(envelope.attack_time));
+      instGenList.genAmount.shAmount = envelope.attack_time;
       memcpy(igenCk->data + dataPtr, &instGenList, sizeof(sfInstGenList));
       dataPtr += sizeof(sfInstGenList);
 
       // holdVolEnv
       instGenList.sfGenOper = holdVolEnv;
-      instGenList.genAmount.shAmount =
-          (envelope.hold_time == 0) ? -32768 : std::round(SecondsToTimecents(envelope.hold_time));
+      instGenList.genAmount.shAmount = envelope.hold_time;
       memcpy(igenCk->data + dataPtr, &instGenList, sizeof(sfInstGenList));
       dataPtr += sizeof(sfInstGenList);
 
       // decayVolEnv
       instGenList.sfGenOper = decayVolEnv;
-      instGenList.genAmount.shAmount =
-          (envelope.decay_time == 0) ? -32768 : std::round(SecondsToTimecents(envelope.decay_time));
+      instGenList.genAmount.shAmount = envelope.decay_time;
       memcpy(igenCk->data + dataPtr, &instGenList, sizeof(sfInstGenList));
       dataPtr += sizeof(sfInstGenList);
 
       // sustainVolEnv
       instGenList.sfGenOper = sustainVolEnv;
-      if (envelope.sustain_level > 100.0)
-        envelope.sustain_level = 100.0;
-      instGenList.genAmount.shAmount = static_cast<int16_t>(envelope.sustain_level * 10);
+      instGenList.genAmount.shAmount = static_cast<int16_t>(envelope.sustain_level);
       memcpy(igenCk->data + dataPtr, &instGenList, sizeof(sfInstGenList));
       dataPtr += sizeof(sfInstGenList);
 
       // releaseVolEnv
       instGenList.sfGenOper = releaseVolEnv;
-      instGenList.genAmount.shAmount =
-          (envelope.release_time == 0) ? -32768 : std::round(SecondsToTimecents(envelope.release_time));
+      instGenList.genAmount.shAmount = envelope.release_time;
       memcpy(igenCk->data + dataPtr, &instGenList, sizeof(sfInstGenList));
       dataPtr += sizeof(sfInstGenList);
 
